2a02010f3afc9fc3026190b6ee8368c60994bc2e
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-03 20:58:47 +0300

    Структура компонента Vue, выведен текст с тегами

--- MakoVueExercises/src/App.vue
+++ MakoVueExercises/src/App.vue
@@ -1,48 +1,28 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div>
+        Hello, World!
+        <!-- Количество: {{ count }} -->
+        <h1> Написать какой-нибудь текст с тегами</h1>
+        <b> Написать какой-нибудь текст с тегами</b>
+        <h2> Написать какой-нибудь текст с тегами</h2>
+        <b> </b>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-    <b>FFFFFFFFFFFFFFFFFFFFFFFF</b>
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+
+<script>
+export default {
+    name: 'App',
+
+     data() {
+        return{
+            count: 0,
+        }
+    }
 }
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+<style>
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>

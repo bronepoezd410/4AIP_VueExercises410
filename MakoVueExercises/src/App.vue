79d97200cdd2cb3084674dff80361f694c1dfedd
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-04 14:42:54 +0300

    014_Реактивность в фреймворке

--- MakoVueExercises/src/App.vue
+++ MakoVueExercises/src/App.vue
@@ -1,7 +1,21 @@
-
 <template>
-	<button @click="show(2)">Получить квадрат числа два</button>
-	<button @click="show(3)">Получить квадрат числа три</button>
+
+	<div>
+        <button @click="change">Изменить</button> 
+        <button @click="change1">Изменить 2</button> 
+        {{ text }}
+    </div>
+
+    <div>
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, cupiditate harum facere quia tempore voluptatibus, inventore ex impedit ipsa minima itaque. Quisquam ad tempora <strong>{{ text }}</strong> quia sit aut nobis nemo officiis?
+
+    </div>
+
+
+
+
+
 </template>
 
 <script>
@@ -10,20 +24,31 @@ import { normalizeProps } from 'vue';
 export default {
     data() {
 	    return {
-
+		text: '',
 	    }
     },
-	methods: {
-	show: function(int) {
-        alert(int * int);
-	},
+    methods: {
+	change: function() {
+		this.text = 'Я БЕШЕННЫЙ, БЕГИТЕ';
 
+	},
+    change1: function(){
+        this.text = 'Я НЕНОРМАЛЬНЫЙ'
     }
+}
 	
 	}
 
 </script>
 
 <style>
+body{
+    background-color: rgb(125, 249, 249);
+}
+div{
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 25px;
+    
+}
 
 </style>

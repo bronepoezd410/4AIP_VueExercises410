69e2cc94003c61f6b1b5bd74db4648e3a00fc240
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-04 16:20:50 +0300

    017_Показ по условию

--- MakoVueExercises/src/App.vue
+++ MakoVueExercises/src/App.vue
@@ -2,10 +2,9 @@
 
 <!-- обработчик click будет вызван максимум 1 раз -->
 <!-- <a @click.once="doThis"></a> -->
-    <button><a @click.once="plusinteger">{{ integer }}</a></button>
-
-    <button><a @click.prevent href="http://github.com/bronepoezd410/">GitHub</a></button>
-
+<!-- <a v-if="visible" href="http://github.com/bronepoezd410/">GitHub</a> -->
+    <p v-if = "visible">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, sit. Veritatis quidem illo, aspernatur distinctio nisi quaerat ab dolor eius incidunt recusandae consequuntur, officia hic, dolores sunt nam similique iure deserunt quis rerum ratione. Culpa maiores dolorum tenetur natus earum.</p>
+    <p v-else = "visible">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Possimus quis numquam quisquam.</p>
 
 
 </template>
@@ -16,16 +15,11 @@ import { normalizeProps } from 'vue';
 export default {
     data() {
 	return {
-        integer: 13
+        visible: true
 	}
     
 },
-    methods: {
-        plusinteger: function(){
-            this.integer = this.integer * 2;
-
-        }
-    },
+    methods: {},
     computed: {}
 }
 
@@ -33,7 +27,7 @@ export default {
 
 <style>
 body{
-    background-color: rgb(64, 5, 65);
+    background-color: rgb(161, 255, 162);
 }
 div{
     font-family: Verdana, Geneva, Tahoma, sans-serif;

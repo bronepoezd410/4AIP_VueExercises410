39bd241b4ddea66e7ab413900e84628d5e2e9070
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-12 15:27:24 +0300

    024_Директива v-else-if

--- MakoVueExercises/src/App.vue
+++ MakoVueExercises/src/App.vue
@@ -1,12 +1,8 @@
 
 <template>
-	<p v-if="day === 1">Понедельник</p>
-	<p v-if="day === 2">Вторник</p>
-	<p v-if="day === 3">Среда</p>
-	<p v-if="day === 4">Четверг</p>
-	<p v-if="day === 5">Пятница</p>
-	<p v-if="day === 6">Суббота</p>
-	<p v-if="day === 7">Воскресенье</p>
+	<p v-if="age < 18">Подросток</p>
+	<p v-else-if="age < 26">Молодой человек</p>
+	<p v-else>Мужчина</p>
 </template>
 
 <script>
@@ -15,7 +11,7 @@ import { normalizeProps } from 'vue';
 export default {
     data() {
 	return {
-		day: 4,
+        age: 25,
 	}
 },
     methods: 

1f0dea3eb50f53ac64a8bc82efb55fd4d6e5868a
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-22 12:24:12 +0300

    050_Формирование селекта в цикле

--- MakoVueExercises/src/App.vue
+++ MakoVueExercises/src/App.vue
@@ -1,19 +1,20 @@
 
 <template>
     <div class="withMargin"> 
-        <p>В каком городе вы живете?</p>
-        <select v-model="selected">
-		<option>Саратов</option>
-		<option>Брянск</option>
-		<option>Калуга</option>
-	    </select>
-	
-	<p>{{ selected }}</p>
 
-    <p v-if="choice == 'Английский'"></p>
+    <select v-model="selectedDay">
+      <option v-for="day in days">{{ day }}</option>
 
 
+    </select>
 
+    <select v-model="selectedMonth">
+      <option v-for="(month, index) in months" :key="index" :value="index">{{ month }}</option>
+    </select>
+
+    <select v-model="selectedYear">
+      <option v-for="year in years">{{ year }}</option>
+    </select>
     </div>
 
 
@@ -23,16 +24,40 @@
 //import { normalizeProps } from 'vue';
 
 export default {
-    data() {
-        return {
-            selected: 'value1', // значение по умолчанию
-        	}
-},
+  data() {
+    // Какое задание такой и ответ.
+    const today = new Date();
+    const daysInMonth = new Date(today.getFullYear(), today.getMonth() + 1, 0).getDate();
+    const daysArray = Array.from({ length: daysInMonth }, (_, i) => i + 1);
+    const yearsArray = Array.from({ length: 10 }, (_, i) => today.getFullYear() - i);
+
+    return {
+      selectedDay: today.getDate(),
+      selectedMonth: today.getMonth(),
+      selectedYear: today.getFullYear(),
+      days: daysArray,
+      months: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ],
+      years: yearsArray,
+    };
+  },
+}
     methods: 
     {
 
     }
-}
 
 
 </script>

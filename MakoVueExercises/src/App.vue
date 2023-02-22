2d4f1bafc1a4366acb176ab25fe64320a5b2c5ab
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-22 12:14:19 +0300

    049_Работа с селектами

--- MakoVueExercises/src/App.vue
+++ MakoVueExercises/src/App.vue
@@ -1,44 +1,16 @@
 
 <template>
     <div class="withMargin"> 
-    <p>
-        Какой язык для вас родной?
-    </p>
-    <input name="radio" type="radio" v-model="choice" value="Русский"> Русский
-	<input name="radio" type="radio" v-model="choice" value="Немецкий"> Немецкий
-	<input name="radio" type="radio" v-model="choice" value="Английский"> Английский
-    <br>
+        <p>В каком городе вы живете?</p>
+        <select v-model="selected">
+		<option>Саратов</option>
+		<option>Брянск</option>
+		<option>Калуга</option>
+	    </select>
+	
+	<p>{{ selected }}</p>
 
-    you choosed: {{ choice }}
-
-    <p v-if="choice == 'Русский'">
-        Россия — священная наша держава, 
-        Россия — любимая наша страна.
-        Могучая воля, великая слава —
-        Твоё достоянье на все времена!
-        Славься, Отечество наше свободное,
-        Братских народов союз вековой,
-        Предками данная мудрость народная!
-        Славься, страна! Мы гордимся тобой!</p>
-    <p v-if="choice == 'Немецкий'">
-        Einigkeit und Recht und Freiheit
-        für das deutsche Vaterland!
-        Danach lasst uns alle streben
-        brüderlich mit Herz und Hand!
-        Einigkeit und Recht und Freiheit
-        sind des Glückes Unterpfand!
-        Blüh im Glanze dieses Glückes,
-        blühe, deutsches Vaterland!
-        Blüh im Glanze dieses Glückes,
-        blühe, deutsches Vaterland!</p>
-    <p v-if="choice == 'Английский'">
-        God save our gracious King (Queen)!
-        Long live our noble King (Queen)!
-        God save the King (Queen)!s
-        Send him (her) victorious,
-        Happy and glorious,
-        Long to reign over us:
-        God save the King (Queen).</p>
+    <p v-if="choice == 'Английский'"></p>
 
 
 
@@ -53,7 +25,7 @@
 export default {
     data() {
         return {
-            choice: '',
+            selected: 'value1', // значение по умолчанию
         	}
 },
     methods: 

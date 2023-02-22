f62a1a5b02b0a3dc46eb9fcfc7c7dfbb07fcdc5f
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-22 11:45:08 +0300

    045_Работа с textarea

--- MakoVueExercises/src/App.vue
+++ MakoVueExercises/src/App.vue
@@ -1,27 +1,14 @@
 
 <template>
     <div class="withMargin"> 
-
-        <input type="number" v-model="num" class="my-input">
-        <button v-on:click="calc">Квадратировать </button>
-        <p>Квадрат введенного числа: {{ res }}</p>
+        <textarea v-model="input"></textarea>
+        <button @click="submit">Submit</button>
+    <ul>
+      <li v-for="word in words" :key="word">{{ word }}</li>
+      <!-- не помню для чего конкретно служит :key -->
+    </ul>
     </div>
 
-    <div class="withMargin">
-        <input type="number" v-model="number1" class="my-input">
-        <input type="number" v-model="number2" class="my-input">
-        <button v-on:click="summ">Сложить </button>
-        <p>Сумма введенных чисел: {{ resnumber }}</p>
-
-    </div>
-
-        
-    <div class="withMargin">
-        <input type="text" v-model="word" class="my-input">
-        <input type="text" v-model="word1" class="my-input">
-        <button v-on:click="shuffle">Поменять </button>
-        <p>Поменяны слова местами {{ word }} {{ word1 }}</p>
-    </div>
 </template>
 
 <script>
@@ -29,34 +16,28 @@
 
 export default {
     data() {
-	return {
-		num: 0,
-		res: 0,
-
-        number1:0,
-        number2:0,
-        resnumber: 0,
-
-        word: '',
-        word1:'',
-        word2:'ALOGA'
+        return {
+        input: '',
+        words: [],
 	}
 },
     methods: 
     {
-        calc: function(){
-        this.res = this.num ** 2
-        },
-        summ: function(){
-            this.resnumber = this.number1 +  this.number2
-        },
-        shuffle: function(){
-            this.word2 = this.word
-            this.word = this.word1;
-            this.word1 = this.word2
-        }
+        submit() {
+      // Split the input into an array of words and update the words data property
+      this.words = this.input.split(' ');
+    },
     }
 }
 
 
 </script>
+
+<style>
+
+@import url('./styles/baton_styles.css');
+@import url('./styles/page.css');
+@import url('./styles/style.css');
+
+
+</style>
--- MakoVueExercises/src/main.js
+++ MakoVueExercises/src/main.js
@@ -1,9 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import Home from './Home.vue'
-import Style from './Styles.vue'
 
 
 createApp(App).mount('#app')
 createApp(Home).mount('#home')
-createApp(Style).mount('#style')

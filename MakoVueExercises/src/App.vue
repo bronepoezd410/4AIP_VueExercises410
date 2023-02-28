01228d43f6fc852051d42ba496c1c90ab3a19f89
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-28 22:22:01 +0300

    069_Реактивное редактирование данных компонента

--- MakoVueExercises/src/App.vue
+++ MakoVueExercises/src/App.vue
@@ -1,3 +1,17 @@
+
+
+<template>
+<Rabotnik v-for="user in users"
+		:id="user.id"
+		:name="user.name"
+		:surn="user.surn"
+		:key="user.id"
+    @change="change"
+/>
+</template>
+
+
+
 <script>
 import Rabotnik from './components/Employee.vue'
 export default {
@@ -19,35 +33,25 @@ export default {
         },
         {
           id: 3,
-          name: 'Виталий',
-          surn: 'Шагин'
+          name: 'Эдем',
+          surn: 'Османов'
         },
       ],
     }
   },
   methods: {
-    remove(id) {
-      this.users = this.users.filter((user) => {
-        return user.id !== id;
-      })
+    change(id, name, surn){
+      this.users = this.users.map((user) => {
+        if(user.id === id){
+          user.name = name;
+          user.surn = surn;
+        }
+        return user;
+      });
     }
   }
 }
 </script>
-
-<template>
-  <Rabotnik
-		v-for="user in users"
-		:id="user.id"
-		:name="user.name"
-		:surn="user.surn"
-		@remove="remove"
-		:key="user.id"
-	/>
-</template>
-
-
-
 
 
 
--- MakoVueExercises/src/components/Employee.vue
+++ MakoVueExercises/src/components/Employee.vue
@@ -1,3 +1,21 @@
+
+<template>
+	<template v-if="!isEdit">
+	  <div class="block">
+		<h1>{{ name }}</h1>
+		<h1>{{ surn }}</h1>
+		<button class="btn" @click="edit">edit</button>
+	  </div>
+	</template>
+	<template v-else>
+	  <div class="block">
+		<input class="block_input" v-model="newName">
+		<input class="block_input" v-model="newSurn">
+		<button class="btn" @click="save">save</button>
+	  </div>
+	</template>
+  </template>
+
 <script>
 export default {
   props: {
@@ -5,22 +23,26 @@ export default {
     name: String,
     surn: String,
   },
-  emits: ['remove'],
+  emits: ['change'],
   data() {
     return {
+      isEdit: false,
+      newName: this.name,
+      newSurn: this.surn,
     }
   },
+  methods: {
+    edit() {
+      this.isEdit = true;
+    },
+    save() {
+      this.isEdit = false;
+      this.$emit('change', this.id, this.newName, this.newSurn);
+    }
+  }
 }
 </script>
 
-<template>
-  <div class="block">
-    {{ name }}
-    {{ surn }}
-    <button class="btn" @click="$emit('remove', id)">remove</button>
-  </div>
-  
-</template>
 
 
 // <!-- props: {

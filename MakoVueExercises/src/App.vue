9288ebdde9ae7081669edf2f7ea25c96db810f3b
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-28 19:25:37 +0300

    057_Работа с компонентами]

--- MakoVueExercises/src/App.vue
+++ MakoVueExercises/src/App.vue
@@ -1,32 +1,9 @@
 <template>
 
-<div>
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Name</th> 
-          <th>Surname</th> 
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="user in users" :key="user.id">
-        <template v-if="!user.isEdit">
-          <td>{{ user.id }}</td>
-          <td>{{ user.name }}</td>
-          <td>{{ user.surn }}</td>
-          <button @click="edit(user)"> edit </button>
-        </template>
-        <template v-else>
-                <td>{{ user.id }}</td>
-				<td> <input v-model="user.name"> </td>
-				<td>  <input v-model="user.surn"> </td>
-                <td>  <button @click="save(user)">save</button></td>
-			</template>
-        </tr>
-      </tbody>
-    </table>
-  </div>
+    <div class="withMargin">
+        <p>промежуточная ветвь </p>
+
+    </div>
 
 
 </template>
@@ -37,36 +14,11 @@ import { normalizeProps } from 'vue';
 export default {
     data() {
 	return {
-		users: [
-			{
-				id: 1,
-				name: 'name1',
-				surn: 'surn1',
-				isEdit: false,
-			},
-			{
-				id: 2,
-				name: 'name2',
-				surn: 'surn2',
-				isEdit: false,
-			},
-			{
-				id: 3,
-				name: 'name3',
-				surn: 'surn3',
-				isEdit: false,
-			},
-		]
+		
 	}
 },
     methods: 
     {
-        edit(user) {
-		user.isEdit = true;
-	},
-	    save(user) {
-		user.isEdit = false;
-	},
     }
 
 }

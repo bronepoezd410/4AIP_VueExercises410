9218eafb458fe07d0a4fc38294a1c60c0277a1b2
Demiurg <danyushis.m.i.21@gmail.com>
2023-02-28 20:36:21 +0300

    062_Указание типа пропсов

--- MakoVueExercises/src/components/Employee.vue
+++ MakoVueExercises/src/components/Employee.vue
@@ -3,13 +3,26 @@
 </template>
 
 
-
 <script>
 export default {
-    props: ['name', 'salary', 'age'],
+	props: {
+		name: String,
+		salary: String,
+		age: Number,
+	},
     data() {
         return {
         }
     }
 }
 </script>
+
+<!-- props: {
+	title: String,
+	likes: Number,
+	isPublished: Boolean,
+	commentIds: Array,
+	author: Object,
+	callback: Function,
+	contactsPromise: Promise // или любой другой конструктор
+} -->
